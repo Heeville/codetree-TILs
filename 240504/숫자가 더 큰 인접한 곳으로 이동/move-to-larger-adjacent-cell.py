63daaa7e713468b2698b0135f478d992e4da6393
jinuk_hong <122798322+Heeville@users.noreply.github.com>
2024-05-04 17:15:09 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240504/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240504/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,10 +1,10 @@
 def move(grid, r, c):
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우 우선순위
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  
     n = len(grid)
     result = []
     while True:
-        result.append(grid[r][c])  
-        max_value = grid[r][c] - 1  
+        result.append(grid[r][c]) 
+        max_value = grid[r][c]   
         next_r, next_c = None, None
         for dr, dc in directions:
             nr, nc = r + dr, c + dc
@@ -12,16 +12,16 @@ def move(grid, r, c):
             if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] > grid[r][c] and grid[nr][nc] > max_value:
                 next_r, next_c = nr, nc
                 max_value = grid[nr][nc]
+                break
 
         if next_r is None or next_c is None:
             break
         r, c = next_r, next_c  
     return result
 
-
 n, r, c = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-
+# 이동 함수 호출하여 결과 출력
 result = move(grid, r - 1, c - 1)
 print(' '.join(map(str, result)))
