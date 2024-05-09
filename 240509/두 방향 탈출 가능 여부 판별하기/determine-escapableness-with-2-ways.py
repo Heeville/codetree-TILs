e3770963d262cbe0eee33386de475a9592aec314
jinuk_hong <122798322+Heeville@users.noreply.github.com>
2024-05-09 23:55:32 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240509/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240509/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,16 +1,27 @@
+from collections import deque
+
 def escape_possible(n, m, area):
-    def dfs(x, y):
-        if x == n - 1 and y == m - 1:
-            return True
-        
-        if x + 1 < n and area[x + 1][y] == 1 and dfs(x + 1, y):
-            return 1
-        if y + 1 < m and area[x][y + 1] == 1 and dfs(x, y + 1):
-            return 1
-        
-        return 0
+    dx = [ 1, 0]
+    dy = [0,  1]
+
+    queue = deque([(0, 0)])
     
-    return dfs(0, 0)
+    while queue:
+        x, y = queue.popleft()
+        
+        if x == n - 1 and y == m - 1:
+            return 1
+        
+        for i in range(2):
+            nx, ny = x + dx[i], y + dy[i]
+            
+            if nx < 0 or nx >= n or ny < 0 or ny >= m or area[nx][ny] == 0:
+                continue
+            
+            area[nx][ny] = 0
+            queue.append((nx, ny))
+    
+    return 0
 
 n, m = map(int, input().split())
 area = [list(map(int, input().split())) for _ in range(n)]
