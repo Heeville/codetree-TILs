9a9a9a7288c718a73c0a2a63cc5b77ee7ee303b8
jinuk_hong <122798322+Heeville@users.noreply.github.com>
2024-05-10 17:38:43 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 149ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240510/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def escape_possible(n, m, area):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    queue = deque([(0, 0)])
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        if x == n - 1 and y == m - 1:
+            return True
+        
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            
+            if nx < 0 or nx >= n or ny < 0 or ny >= m or area[nx][ny] == 0:
+                continue
+
+            area[nx][ny] = 0
+            queue.append((nx, ny))
+
+    return False
+
+
+n, m = map(int, input().split())
+area = [list(map(int, input().split())) for _ in range(n)]
+
+
+result = escape_possible(n, m, area)
+print(result)
